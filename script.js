42704225fe7a03bb3b2ef0476d8185ec3a755332
Shobhi Maheshwari <shobhi094@gmail.com>
2018-04-06 22:34:27 -0600

    Fixed navbar

--- script.js
+++ script.js
@@ -3,53 +3,25 @@
         var navOffset = jQuery(".background-white").offset().top;
 //        alert(navOffset);
         $(window).scroll(function(){                          
-//            if ($(this).scrollTop() > navOffset) {
-//                $('.header').fadeIn(500);
-//            } else {
-//                $('.header').fadeOut(500);
-//            }       
+            if ($(this).scrollTop() > navOffset) {
+                $('.header').css("opacity","1");
+                $('.header').fadeIn(300);
+            } else {
+                $('.header').css("opacity","0");
+                $('.header').fadeOut(300);
+            }       
             
-            $('.header').each(function() {
-                var top_of_window = $(window).scrollTop();
-                if (top_of_window > navOffset) {
-//                    $(this).fadeTo(500,1);
-//                    $(this).animate({'opacity':'1'},1500);
-                    if ($(this).css('opacity')==0) {$(this).fadeTo(500,1);}
-                } else {
-//                    $(this).fadeTo(500,0);
-                    if ($(this).css('opacity')==1) {$(this).fadeTo(500,0);}
-                }
-            });
+//            $('.header').each(function() {
+//                var top_of_window = $(window).scrollTop();
+//                if (top_of_window > navOffset) {
+////                    $(this).fadeTo(500,1);
+////                    $(this).animate({'opacity':'1'},1500);
+//                    if ($(this).css('opacity')==0) {$(this).fadeTo(500,1);}
+//                } else {
+////                    $(this).fadeTo(500,0);
+//                    if ($(this).css('opacity')==1) {$(this).fadeTo(500,0);}
+//                }
+//            });
         });
     });
 })(jQuery);
-
-//jQuery(document).ready(function() {
-//   var navOffset = jQuery(".nav-wrapper").offset().top;
-//    
-//    jQuery(window).scroll(function() {
-//        var scrollPos = jQuery(window).scrollTop();
-//        jQuery(".status").html(scrollPos);
-//        
-////        if (scrollPos > navOffset) {
-////            jQuery(".nav-wrapper").addClass("fixed");
-////        } else {
-////            jQuery(".nav-wrapper").addClass("fixed");
-////        }
-//    });
-//    
-//    
-//    
-//});
-
-//// When the user scrolls the page, execute myFunction 
-//window.onscroll = function() {myFunction()};
-//
-//// Get the navbar
-//var navbar = document.getElementsByClassName("nav-wrapper");
-//
-//// Get the offset position of the navbar
-//var sticky = navbar.offsetTop;
-//
-//alert(sticky);
-
