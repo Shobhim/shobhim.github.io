7b974036c995f97c3b85dfd894d5b43852a18cdc
Shobhi Maheshwari <shobhi094@gmail.com>
2018-04-08 12:34:37 -0600

    Added active section functionality.

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-(function($) {          
-    $(document).ready(function(){   
-        var navOffset = jQuery(".background1").offset().top - 2;
-        $(window).scroll(function(){                          
-            if ($(this).scrollTop() > navOffset) {
-                $('.fixed-header').css("z-index","2");
-                $('.fixed-header').fadeIn(500);
-            } else {
-                $('.fixed-header').fadeOut(500);
-            }       
-        });
+$(function() {
+    var navOffset = $(".background1").offset().top - 1;
+    
+    $(document).on('scroll', function() {
+        var scrollTop = $(document).scrollTop();
+        
+        if (scrollTop > navOffset) {
+            $('.fixed-header').css("z-index","2");
+            $('.fixed-header').fadeIn(500);
+        } else {
+            $('.fixed-header').fadeOut(500);
+        }
     });
-})(jQuery);
-
+    
+    var aboutOffsetTop = $('#about').offset().top;
+    var resumeOffsetTop = $('#resume').offset().top;
+    var projectsOffsetTop = $('#projects').offset().top;
+    var contactOffsetTop = $('#contact').offset().top;
+    
+    $(document).on('scroll', function() {
+        var scrollTop = $(document).scrollTop();
+        var activeLi;
+        
+        if (scrollTop >= aboutOffsetTop && scrollTop < resumeOffsetTop) {
+            activeLi = $('.header-items>li:nth-child(1)');
+        } else if (scrollTop >= resumeOffsetTop && scrollTop < projectsOffsetTop) {
+            activeLi = $('.header-items>li:nth-child(2)');
+        } else if (scrollTop >= projectsOffsetTop && scrollTop < contactOffsetTop) {
+            activeLi = $('.header-items>li:nth-child(3)');
+        } else if (scrollTop >= contactOffsetTop) {
+            activeLi = $('.header-items>li:nth-child(4)');
+        }
+        
+        activeLi.addClass('active');
+        $('.header-items>li').not(activeLi).removeClass('active');
+    });
+});
