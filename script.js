77f6c10c20c06d88562bf942624d9dec46de5cb8
Shobhi Maheshwari <shobhi094@gmail.com>
2018-04-08 02:28:16 -0600

    Added Projects section.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 (function($) {          
     $(document).ready(function(){   
         var navOffset = jQuery(".background1").offset().top;
-//        alert(navOffset);
         $(window).scroll(function(){                          
             if ($(this).scrollTop() > navOffset) {
                 $('.fixed-header').css("z-index","2");
@@ -9,18 +8,6 @@
             } else {
                 $('.fixed-header').fadeOut(500);
             }       
-            
-//            $('.header').each(function() {
-//                var top_of_window = $(window).scrollTop();
-//                if (top_of_window > navOffset) {
-////                    $(this).fadeTo(500,1);
-////                    $(this).animate({'opacity':'1'},1500);
-//                    if ($(this).css('opacity')==0) {$(this).fadeTo(500,1);}
-//                } else {
-////                    $(this).fadeTo(500,0);
-//                    if ($(this).css('opacity')==1) {$(this).fadeTo(500,0);}
-//                }
-//            });
         });
     });
 })(jQuery);
